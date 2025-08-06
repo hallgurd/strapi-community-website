c9cb1b7a528497204d60e311cb46d93fcbab2d7d
hallgurd <hallgurd.bakr@gmail.com>
2025-08-06 07:59:58 +0300

    update form

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 export default function Form() {
   return (
     <div className="my-6 text-left space-y-4">
       <label className="block">
         <span className="after:content-['*'] text-left after:ml-0.5 after:text-danger-500 block text-sm font-medium text-neutral-700">
-          Name
+          Full Name
         </span>
         <input
           type="text"
@@ -22,7 +20,7 @@ export default function Form() {
           type="email"
           name="email"
           className="mt-1 px-4 py-3 bg-white border shadow-sm border-neutral-300 placeholder-slate-400 focus:outline-none focus:border-alternative-500 focus:ring-alternative-500 block w-full rounded-md sm:text-sm focus:ring-1"
-          placeholder="you@example.com"
+          placeholder="name@example.com"
         />
       </label>
       <label className="block">
@@ -33,7 +31,7 @@ export default function Form() {
           type="text"
           name="Subject"
           className="mt-1 px-4 py-3 bg-white border shadow-sm border-neutral-300 placeholder-slate-400 focus:outline-none focus:border-alternative-500 focus:ring-alternative-500 block w-full rounded-md sm:text-sm focus:ring-1"
-          placeholder=""
+          placeholder="Subject"
         />
       </label>
       <label className="block">
@@ -44,6 +42,7 @@ export default function Form() {
           rows="6"
           name="Message"
           className="mt-1 px-4 py-3 bg-white border shadow-sm border-neutral-300 placeholder-slate-400 focus:outline-none focus:border-alternative-500 focus:ring-alternative-500 block w-full rounded-md sm:text-sm focus:ring-1"
+          placeholder="Enter your message"
         ></textarea>
       </label>
       <button className="rounded-md bg-primary-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-primary-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600">
