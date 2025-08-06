a98cc5dc73cd50edbf0b53ab67d5d710e9ccfd80
hallgurd <hallgurd.bakr@gmail.com>
2025-08-06 06:18:46 +0300

    Add Grid and redesign

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import RippleGrid from "./RippleGrid";
+
 export default function Hero() {
   return (
     <div className="h-screen relative px-6 md:px-8 -mt-16 flex items-center justify-center">
-      <div className="mx-auto max-w-2xl text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-darkPrimary sm:text-6xl">
-            Welcome to Strapi Community
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-neutral-600">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-            cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-          </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-primary-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-primary-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-            >
-              Get started
-            </a>
-            <a href="https://docs.strapi.io/" className="text-base font-semibold leading-7 text-darkPrimary">
-            Docs <span aria-hidden="true">→</span>
-            </a>
+      <div className="mx-auto max-w-2xl text-center z-10">
+        <h1 className="text-4xl font-bold tracking-tight text-darkPrimary sm:text-6xl">
+          Welcome to Strapi Community
+        </h1>
+        <p className="mt-6 text-lg leading-8 text-neutral-600">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
+          commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-8">
+          <Link
+            to="/showcase"
+            className="rounded-lg bg-primary-600 px-5 py-3 text-base font-semibold leading-7 text-white shadow-sm hover:bg-primary-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+          >
+            Get started
+          </Link>
+          <Link
+            to="/about"
+            className="rounded-lg px-5 py-3 text-base font-semibold leading-7 text-darkPrimary border bg-white border-primary-200 hover:bg-primary-100"
+          >
+            About Us
+          </Link>
         </div>
+      </div>
+
+      <div className="absolute overflow-hidden h-[700px] top-10 opacity-20" >
+        <RippleGrid
+          enableRainbow={false}
+          gridColor="#c4c2ff"
+          rippleIntensity={0.05}
+          gridSize={30}
+          gridThickness={40}
+          mouseInteraction={false}
+          mouseInteractionRadius={1.2}
+          opacity={0.7}
+        />
       </div>
     </div>
   );
